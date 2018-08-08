5d9326c369ef2353fd7d452763113a685fcf79e3
Ubuntu <ubuntu@ip-172-31-3-197.us-west-1.compute.internal>
2018-08-08 18:54:20 +0000

    Fixed pdf serve

--- scripts/server.js
+++ scripts/server.js
@@ -3,21 +3,24 @@ var http = require('http');
 var url = require('url');
 
 http.createServer(function (req, res) {
-	
-	var q = url.parse(req.url, true);
-	var file = "." + q.pathname;
-	if (file == "./"){
-		file = "index.html";
+
+	var q = url.parse(req.url,true);
+	var file = ".." + q.pathname;
+	if (file == ".."){
+		file = "/index.html";
 	}
 
 	var type = 'text/html';
 	if (file.indexOf('.css') != -1) {
 		type = 'text/css';
-	} 
+	} else if (file.indexOf('.pdf') != -1) {
+		type = 'application/pdf';
+	}
+	console.log(file);
 	fs.readFile(file, function(err,data) {
 		if (err) {
 			res.writeHead(404, {'Content-Type': 'text/html'});
-			return res.end("404");
+			return res.end("404 Not Found");
 		}
 		res.writeHead(200, {'Content-Type': type});
 		res.write(data);
--- scripts/side_menu.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $.get("side_menu.html", function(data, status){
-        $("#side_bar").append(data);
-        console.log(data);
-        console.log(status);
-
-    });
-});
