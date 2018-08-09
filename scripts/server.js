c76a24009c01c37df273c8e191807b86b66dc4df
Ubuntu <ubuntu@ip-172-31-3-197.us-west-1.compute.internal>
2018-08-09 22:46:41 +0000

    Testing

--- scripts/server.js
+++ scripts/server.js
@@ -6,8 +6,8 @@ http.createServer(function (req, res) {
 
 	var q = url.parse(req.url,true);
 	var file = ".." + q.pathname;
-	if (file == ".."){
-		file = "/index.html";
+	if (file == "../"){
+		file = "../index.html";
 	}
 
 	var type = 'text/html';
@@ -20,7 +20,7 @@ http.createServer(function (req, res) {
 	fs.readFile(file, function(err,data) {
 		if (err) {
 			res.writeHead(404, {'Content-Type': 'text/html'});
-			return res.end("404 Not Found");
+			return res.end("404 Not Found!");
 		}
 		res.writeHead(200, {'Content-Type': type});
 		res.write(data);
