2771f45132c0b2024d495ce377c809208f707dc0
Daniel <dmartinez79915@gmail.com>
2018-08-21 18:18:01 -0700

    Redesigned into single page

--- scripts/every.js
+++ scripts/every.js
@@ -1,9 +1,4 @@
-var path = location.pathname;
-var name = path.split("/").pop();
-var identity = name.split(".");
-console.log(identity[0]);
+
 $(document).ready(function () {
-    $("#navdiv").load("nav_bar.html", function () {
-        $("#" + identity).addClass("current");
-    });
+    $("#navdiv").load("nav_bar.html")
 }); 
