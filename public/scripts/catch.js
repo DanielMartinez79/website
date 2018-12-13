f1abab2344340b2976abbc77fe7c036788e61c05
Daniel <dmartinez79915@gmail.com>
2018-12-12 22:42:35 -0800

    test

--- public/scripts/catch.js
+++ public/scripts/catch.js
@@ -148,14 +148,15 @@ router.get("/test", function (req, res){
 */
 router.post("/recap", function(req,res) {
     console.log(req.body['g-recaptcha-response'])
-
+    console.log(req.body.token)
     request.post("https://www.google.com/recaptcha/api/siteverify", {
         form: {
             secret: "6LcJcIAUAAAAAEw1yqrXhO9p-fcq6nMkTMylAzmV",
             response: req.body['g-recaptcha-response']
         }
     }, function(error, response, body) {
-        
+        res.send(response)
+        res.send(body)
     })
 
     res.end();
